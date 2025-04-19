401b455a70a3bfb41c27f96cadc0f35312d1328a
LuxJakob <112105354+LuxJakob@users.noreply.github.com>
2025-04-19 22:11:22 +0200

    added missing postgrest attributes

--- vitalStatsVB/HealthRecord.vb
+++ vitalStatsVB/HealthRecord.vb
@@ -1,21 +1,28 @@
 ﻿Imports System.Text.Json.Serialization
 Imports Supabase.Postgrest.Models
+Imports Supabase.Postgrest.Attributes
 
+<Table("health_records")>
 Public Class HealthRecord
     Inherits BaseModel
 
+    <PrimaryKey("date", False)>
+    <Column("date")>
     <JsonPropertyName("date")>
     Public Property Timestamp As DateTime
 
+    <Column("weight_kg")>
     <JsonPropertyName("weight_kg")>
     Public Property Weight As Double
 
+    <Column("amount_donated_ml")>
     <JsonPropertyName("amount_donated_ml")>
     Public Property AmountDonated As Integer
 
     <JsonIgnore>
     Public Property BloodPressure As String
 
+    <Column("blood_pressure_upper")>
     <JsonPropertyName("blood_pressure_upper")>
     Public ReadOnly Property BloodPressureUpper As Integer
         Get
@@ -24,6 +31,7 @@ Public Class HealthRecord
         End Get
     End Property
 
+    <Column("blood_pressure_lower")>
     <JsonPropertyName("blood_pressure_lower")>
     Public ReadOnly Property BloodPressureLower As Integer
         Get
