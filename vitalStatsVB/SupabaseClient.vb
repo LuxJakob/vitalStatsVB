d1ab8a679d959a44a05aedfe310b486e31b10119
LuxJakob <112105354+LuxJakob@users.noreply.github.com>
2025-04-19 12:52:57 +0200

    initialize Supabase DB

---
+++ vitalStatsVB/SupabaseClient.vb
@@ -0,0 +1,16 @@
+﻿Imports Supabase
+Imports Supabase.Gotrue
+Imports Postgrest
+
+Public Module SupabaseClient
+    Private ReadOnly SupabaseUrl As String = "YOUR_SUPABASE_URL"
+    Private ReadOnly SupabaseKey As String = "YOUR_SUPABASE_KEY"
+    Private Client As Supabase.Client
+
+    Public Async Function Initialize() As Task
+        Client = New Supabase.Client(SupabaseUrl, SupabaseKey)
+        Await Client.InitializeAsync()
+    End Function
+
+
+End Module
